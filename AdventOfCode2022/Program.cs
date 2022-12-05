1eb748dec6d8e42b4088c66960db4f85dcde9a39
Kevin Stoddard <kstddrd@gmail.com>
2022-12-04 20:28:54 -0500

    lotsabad12042022828p

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,20 @@
 ﻿using AdventOfCode2022.Week1.Day1;
 
+using System.Security.Cryptography.X509Certificates;
+
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine($"{Day1Code.codetest}");
+Console.WriteLine(Day1Code.codetest);
+string check = Day1Code.codetest.Replace("\n", "");
+check = check.Replace("\r", "");
+List<string> newtest = Day1Code.codetest.Split("\n").ToList();
+
+Console .WriteLine(newtest.Count);
+List<int> newnewtest = new List<int>();
+foreach(string test in newtest)
+{
+    if (test != "")
+    {
+        newnewtest.Add(int.Parse(test));
+    }
+}
+Console.WriteLine(newtest.Count);
--- AdventOfCode2022/Week1/Day1/Day1Code.cs
+++ AdventOfCode2022/Week1/Day1/Day1Code.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode2022.Week1.Day1
 {
     
     public class Day1Code
     {
-        public static string codetest => "cow";
+        public static string fpath => @"C:\Users\kstdd\source\repos\AdventOfCode2022\AdventOfCode2022\Week1\Day1\testData.txt";
+        public static string codetest => File.ReadAllText(fpath);
+        
     }
 }
